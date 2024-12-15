a187e65cad8c7f473736c8af0651de91eec1de0c
charlotte2078 <150293132+charlotte2078@users.noreply.github.com>
2024-12-15 16:49:12 +0000

    Add project files.

---
+++ MathHelper.h
@@ -0,0 +1,94 @@
+// Code based on https://github.com/microsoft/DirectX-Graphics-Samples/blob/master/MiniEngine/Core/Math/Random.h
+// and Frank Luna https://github.com/d3dcoder/d3d12book/blob/master/Common/MathHelper.h
+
+#pragma once
+
+#include <Windows.h>
+#include <DirectXMath.h>
+#include <cstdint>
+
+class MathHelper
+{
+public:
+	// Returns random float in [0, 1)
+	static float RandF()
+	{
+		return static_cast<float>(rand()) / static_cast<float>(RAND_MAX); // replaced C-style casts
+	}
+
+	// Returns random float in [a, b)
+	static float RandF(float a, float b)
+	{
+		return a + RandF() * (b - a);
+	}
+
+	static int Rand(int a, int b)
+	{
+		return a + rand() % ((b - a) + 1);
+	}
+
+	template<typename T>
+	static T Min(const T& a, const T& b)
+	{
+		return a < b ? a : b;
+	}
+
+	template<typename T>
+	static T Max(const T& a, const T& b)
+	{
+		return a > b ? a : b;
+	}
+
+	template<typename T>
+	static T Lerp(const T& a, const T& b)
+	{
+		return a + (b - a) * t;
+	}
+
+	template<typename T>
+	static T Clamp(const T& x, const T& low, const T& high)
+	{
+		return x < low ? low : (x > high ? high : x);
+	}
+
+	// Returns the polar angle of the point (x,y) in [0, 2*PI)
+	static float AngleFromXY(float x, float y);
+
+	static DirectX::XMVECTOR SphericalToCartesian(float radius, float theta, float phi)
+	{
+		return DirectX::XMVectorSet(
+			radius * sin(phi) * cos(theta),
+			radius * cos(phi),
+			radius * sin(phi) * sin(theta),
+			1.0f);
+	}
+
+	static DirectX::XMMATRIX InverseTranspose(DirectX::CXMMATRIX M)
+	{
+		// Zero out the translation row because we don't want the inverse transpose of the
+		// translation.
+		DirectX::XMMATRIX A = M;
+		A.r[3] = DirectX::XMVectorSet(0.0f, 0.0f, 0.0f, 1.0f);
+
+		DirectX::XMVECTOR det = DirectX::XMMatrixDeterminant(A);
+
+		return DirectX::XMMatrixTranspose(DirectX::XMMatrixInverse(&det, A));
+	}
+
+	static DirectX::XMFLOAT4X4 Identity4x4(DirectX::CXMMATRIX M)
+	{
+		static DirectX::XMFLOAT4X4 I(
+			1.0f, 0.0f, 0.0f, 0.0f,
+			0.0f, 1.0f, 0.0f, 0.0f,
+			0.0f, 0.0f, 1.0f, 0.0f,
+			0.0f, 0.0f, 0.0f, 1.0f);
+
+		return I;
+	}
+
+	static DirectX::XMVECTOR RandUnitVec3();
+	static DirectX::XMVECTOR RandHemishpereUnitVec3(DirectX::XMVECTOR n);
+
+	static const float Infinity;
+	static const float Pi;
+};
---
+++ d3dUtil.h
@@ -0,0 +1,115 @@
+// Based on code by Frank Luna
+// https://github.com/d3dcoder/d3d12book/blob/master/Common/d3dUtil.h
+
+#pragma once
+
+#include <windows.h>
+#include <wrl.h>
+#include <dxgi1_4.h>
+#include <d3d12.h>
+#include <D3DCompiler.h>
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <DirectXColors.h>
+#include <DirectXCollision.h>
+#include <string>
+#include <memory>
+#include <algorithm>
+#include <vector>
+#include <array>
+#include <unordered_map>
+#include <cstdint>
+#include <fstream>
+#include <sstream>
+#include <cassert>
+#include "d3dx12.h"
+#include "DDSTextureLoader.h"
+#include "MathHelper.h"
+
+extern const int gNumFramResources;
+
+// Functions to set the Debug Object name of a D3D12Device, D3D12DeviceChild, DXGIObject
+inline void d3dSetDebugName(IDXGIObject* obj, const char* name)
+{
+	if (obj)
+	{
+		obj->SetPrivateData(WKPDID_D3DDebugObjectName, lstrlenA(name), name);
+	}
+}
+
+inline void d3dSetDebugName(ID3D12Device* obj, const char* name)
+{
+	if (obj)
+	{
+		obj->SetPrivateData(WKPDID_D3DDebugObjectName, lstrlenA(name), name);
+	}
+}
+
+inline void d3dSetDebugName(ID3D12DeviceChild* obj, const char* name)
+{
+	if (obj)
+	{
+		obj->SetPrivateData(WKPDID_D3DDebugObjectName, lstrlenA(name), name);
+	}
+}
+
+inline std::wstring AnsiToWString(const std::string& str)
+{
+	WCHAR buffer[512];
+	MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, buffer, 512);
+	return std::wstring(buffer);
+}
+
+class d3dUtil
+{
+public:
+	static bool IsKeyDown(int vkeyCode);
+
+	static std::string ToString(HRESULT hr);
+
+	// Ensures constant buffer is a multiple of minimum hardware allocation size
+	// (256 bytes)
+	static UINT CalcConstantBufferByteSize(UINT byteSize)
+	{
+		// Rounds up to the nearest multiple of 256
+		return (byteSize + 255) & ~255;
+	}
+
+	static Microsoft::WRL::ComPtr<ID3DBlob> LoadBinary(const std::wstring& filename);
+
+	static Microsoft::WRL::ComPtr<ID3D12Resource> CreateDefaultBuffer(
+		ID3D12Device* device,
+		ID3D12GraphicsCommandList* cmdList,
+		const void* initData,
+		UINT64 byteSize,
+		Microsoft::WRL::ComPtr<ID3D12Resource>& uploadBuffer);
+
+	static Microsoft::WRL::ComPtr<ID3DBlob> CompileShader(
+		const std::wstring& filename,
+		const D3D_SHADER_MACRO* defines,
+		const std::string& entrypoint,
+		const std::string& target);
+};
+
+// Defines an exception with an error code, function name, file name, and line number
+class DxException
+{
+public:
+	DxException() = default;
+	DxException(HRESULT hr, const std::wstring& functionName, const std::wstring& filename, int lineNumber);
+
+	std::wstring ToString() const;
+
+	HRESULT ErrorCode = S_OK;
+	std::wstring FunctionName;
+	std::wstring Filename;
+	int LineNumber = -1;
+};
+
+// Used whenm multiple geometries are stored in one vertex and index buffer
+struct SubmeshGeometry
+{
+	UINT IndexCount = 0;
+	UINT StartIndexLocation = 0;
+	INT BaseVertexLocation = 0;
+};
